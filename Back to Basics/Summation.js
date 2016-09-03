c15d79db92cb104766f946280544617bc7885c3f
Tom LeConey <tleconey@gmail.com>
2016-09-03 15:43:04 -0500

    converted tabs to spaces

--- Back to Basics/Summation.js
+++ Back to Basics/Summation.js
@@ -3,16 +3,16 @@
 
 // O(n) - linear time complexity
 var summation = function (number) {
-	var sum = 0;
-	for (var i = 0; i <= number; i++) {
-		sum += i;
-	}
-	return sum;
+  var sum = 0;
+  for (var i = 0; i <= number; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 // O(1) - constant time complexity
 var summation = function (number) {
-	return number * (number + 1) / 2;
+  return number * (number + 1) / 2;
 }
 
 // ES2015 syntax
@@ -22,8 +22,8 @@ const summation = number => number * (number + 1) / 2;
 
 // Testing Suite
 describe('summation', function () {
-	it('should return the correct sum', function () {
-		expect(summation(1)).to.equal(1);
-		expect(summation(8)).to.equal(36);
-	})
+  it('should return the correct sum', function () {
+    expect(summation(1)).to.equal(1);
+    expect(summation(8)).to.equal(36);
+  })
 })
