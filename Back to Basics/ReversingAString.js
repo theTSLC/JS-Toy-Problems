343fe99bc73b45dd1c2b452c583c3a21dc3e7fe4
Tom LeConey <tleconey@gmail.com>
2016-09-07 00:02:01 -0500

    converted tabs to spaces until I remember to switch to spaces

--- Back to Basics/ReversingAString.js
+++ Back to Basics/ReversingAString.js
@@ -4,30 +4,30 @@
 // O(n) - linear time complexity
 // Decrementing For Loop
 var reversing = function (string) {
-	var reversed = "";
-	for (var i = string.length - 1; i >= 0; i--) {
-		reversed += string[i];
-	}
-	return reversed;
+  var reversed = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  return reversed;
 }
 
 // Using built in methods
 var reversing = function (string) {
-	var split = string.split("");
-	var reversed = split.reverse();
-	var joined = reversed.join("");
-	return joined;
+  var split = string.split("");
+  var reversed = split.reverse();
+  var joined = reversed.join("");
+  return joined;
 }
 // Curried
 var reversing = function (string) {
-	string.split("").reverse().join("");
+  string.split("").reverse().join("");
 }
 
 // Double loops
 var reversing = function (string) {
-	var array = [];
-	for (var i = string.length - 1; x = 0; i >=0; i--; x++) {
-		array[x] = string[i];
-	}
-	return array.join("");
+  var array = [];
+  for (var i = string.length - 1; x = 0; i >=0; i--; x++) {
+    array[x] = string[i];
+  }
+  return array.join("");
 }
