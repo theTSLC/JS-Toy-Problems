66529fe0e0a00e23672572883c9407175538080a
Tom LeConey <tleconey@gmail.com>
2016-09-06 23:55:56 -0500

    2nd way of reversing a string, using built in methods

--- Back to Basics/ReversingAString.js
+++ Back to Basics/ReversingAString.js
@@ -1,6 +1,8 @@
 // Back to Basics: Reverse a String
 // Reverse a string multiple ways
 
+// O(n) - linear time complexity
+// Decrementing For Loop
 var reversing = function (string) {
 	var reversed = "";
 	for (var i = string.length - 1; i >= 0; i--) {
@@ -9,4 +11,14 @@ var reversing = function (string) {
 	return reversed;
 }
 
-var 
+// Using built in methods
+var reversing = function (string) {
+	var split = string.split("");
+	var reversed = split.reverse();
+	var joined = reversed.join("");
+	return joined;
+}
+// Curried
+var reversing = function (string) {
+	string.split("").reverse().join("");
+}
